b5003d59fff104489749dc6907d02e500b0ac7ef
Ijay Abby <inyangasi4@gmail.com>
2021-03-29 16:49:34 +0300

    Implement Book function

--- index.js
+++ index.js
@@ -1,8 +1,28 @@
-let myLibrary = [];
+let title = document.getElementById('title');
 
-function Book() {
-  // the constructor...
+let author = document.getElementById('author');
+
+let pages = document.getElementById('pages');
+
+let button_read = document.getElementById('button-read');
+
+let add_book = document.getElementById('add-book');
+
+
+
+
+let myLibrary = [];
+let newBook
+let myLibrary
+
+function Book(title, author, pages, read = false) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
 }
+
+
 
 function addBookToLibrary() {
   // do stuff here
